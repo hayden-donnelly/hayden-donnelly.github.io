92da861bcecc2e88f57a2b1a4fb097df3d4196f0
hayden-donnelly <hayden.git@proton.me>
2022-01-27 17:26:10 -0500

    added about me

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const timeline = new TimelineMax();
-timeline.to('.square', 1, {top: "-150%"})
-
 const controller = new ScrollMagic.Controller();
-const scene = new ScrollMagic.Scene({
+
+const timeline1 = new TimelineMax();
+timeline1.fromTo('.about-me', {opacity: 0}, {opacity: 1, duration: 1})
+
+const scene1 = new ScrollMagic.Scene({
+    triggerElement: '.title',
+    triggerHook: 0.4,
+    duration: 100,
+})
+.setTween(timeline1)
+.addTo(controller)
+.addIndicators()
+
+
+const timeline2 = new TimelineMax();
+timeline2.to('.square', 1, {top: "-150%"})
+
+const scene2 = new ScrollMagic.Scene({
     triggerElement: '.trigger',
     triggerHook: 1,
     duration: 800,
 })
-.setTween(timeline)
+.setTween(timeline2)
 .addTo(controller)
 //.addIndicators()
