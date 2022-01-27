21784dbf06983347aa975d68311e6cfde5841113
hayden-donnelly <hayden.git@proton.me>
2022-01-27 02:50:36 -0500

    scroll swipe transition

---
+++ app.js
@@ -0,0 +1,11 @@
+const timeline = new TimelineMax();
+timeline.to('.square', 1, {top: "-150%"})
+
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.trigger',
+    triggerHook: 0.9
+})
+.setTween(timeline)
+.addTo(controller)
+//.addIndicators()
