483de74fc786aeec587e36c1c0b28cce06d36446
hayden-donnelly <donnellyhd@outlook.com>
2022-01-28 20:37:12 -0500

    cleaned up site

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
 const controller = new ScrollMagic.Controller();
 
-const timeline1 = new TimelineMax();
-timeline1.fromTo('.about-me', {opacity: 0}, {opacity: 1, duration: 1})
+const timeline = new TimelineMax();
+timeline.to('.transition', 1, {top: '-100%'})
 
-const scene1 = new ScrollMagic.Scene({
+const scene = new ScrollMagic.Scene({
     triggerElement: '.title',
-    triggerHook: 0.3,
-    duration: 150,
+    triggerHook: 0.22,
+    duration: 400,
 })
-.setTween(timeline1)
+.setTween(timeline)
 .addTo(controller)
 //.addIndicators()
 
-
 const timeline2 = new TimelineMax();
-timeline2.to('.square', 1, {top: "-150%"})
+timeline2.fromTo('.about-me', {opacity: 0}, {opacity: 1, duration: 1})
 
 const scene2 = new ScrollMagic.Scene({
-    triggerElement: '.trigger',
-    triggerHook: 0.85,
-    duration: 800,
+    triggerElement: '.title',
+    triggerHook: 0.35,
+    duration: 75,
 })
 .setTween(timeline2)
 .addTo(controller)
 //.addIndicators()
 
 const timeline3 = new TimelineMax();
-timeline3.fromTo('.projects', {opacity: 0}, {opacity: 1, duration: 1})
+timeline3.fromTo('.header2', {opacity: 0}, {opacity: 1, duration: 1})
 
 const scene3 = new ScrollMagic.Scene({
-    triggerElement: '.trigger',
-    triggerHook: 0.5,
-    duration: 100,
+    triggerElement: '.title',
+    triggerHook: 0.2,
+    duration: 75,
 })
 .setTween(timeline3)
 .addTo(controller)
-//.addIndicators()
--- test-app.js
+++ test-app.js
@@ -1,3 +1,36 @@
 const controller = new ScrollMagic.Controller();
-const test = new TimelineMax();
-test.to('.bg', 1, {y: 500, ease: "elastic"})
+
+const timeline = new TimelineMax();
+timeline.to('.transition', 1, {top: '-100%'})
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.title',
+    triggerHook: 0.3,
+    duration: 400,
+})
+.setTween(timeline)
+.addTo(controller)
+//.addIndicators()
+
+const timeline2 = new TimelineMax();
+timeline2.fromTo('.about-me', {opacity: 0}, {opacity: 1, duration: 1})
+
+const scene2 = new ScrollMagic.Scene({
+    triggerElement: '.title',
+    triggerHook: 0.45,
+    duration: 75,
+})
+.setTween(timeline2)
+.addTo(controller)
+//.addIndicators()
+
+const timeline3 = new TimelineMax();
+timeline3.fromTo('.header2', {opacity: 0}, {opacity: 1, duration: 1})
+
+const scene3 = new ScrollMagic.Scene({
+    triggerElement: '.title',
+    triggerHook: 0.2,
+    duration: 75,
+})
+.setTween(timeline3)
+.addTo(controller)
